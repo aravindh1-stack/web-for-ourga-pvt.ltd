77323bc948353e7a61ec922c544f55319749a710
ARAVINDH MURUGESAN <aravindhofficiallinks@gmail.com>
2024-12-26 00:43:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Simple button animation effect
+const learnMoreButton = document.querySelector('.learn-more');
+learnMoreButton.addEventListener('mouseenter', () => {
+    learnMoreButton.style.transform = 'scale(1.1)';
+});
+learnMoreButton.addEventListener('mouseleave', () => {
+    learnMoreButton.style.transform = 'scale(1)';
+});
+// Simple button animation effect
+const learnMoreButton = document.querySelector('.learn-more');
+learnMoreButton.addEventListener('mouseenter', () => {
+    learnMoreButton.style.transform = 'scale(1.1)';
+});
+learnMoreButton.addEventListener('mouseleave', () => {
+    learnMoreButton.style.transform = 'scale(1)';
+});
+// Simple button animation effect
+const learnMoreButton = document.querySelector('.learn-more');
+learnMoreButton.addEventListener('mouseenter', () => {
+    learnMoreButton.style.transform = 'scale(1.1)';
+});
+learnMoreButton.addEventListener('mouseleave', () => {
+    learnMoreButton.style.transform = 'scale(1)';
+});
